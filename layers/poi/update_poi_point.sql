74ac89878177e16bc2e975dddc9d80eb00eaf751
Handri-Bikemap <handri@bikemap.net>
2022-08-17 10:08:44 +0200

    update poi.sql, poi.yaml and update_poi_point.sql
    -Separated the method that generates the addresses into a new function created in the update_poi_point.sql
    -Added NULLIF functionality so that the additional attributes will not display if it is empty.

--- layers/poi/update_poi_point.sql
+++ layers/poi/update_poi_point.sql
@@ -50,42 +50,19 @@ BEGIN
       AND tags != update_tags(tags, geometry);
 
     UPDATE osm_poi_point as points
-    SET address =
-        CASE
-            WHEN addr_full IS NOT NULL AND addr_full != '' THEN addr_full
-            WHEN country.country_code in ('at', 'de', 'nl', 'no', 'ch', 'pl') THEN concat_ws(',',
-                concat_ws(' ' , tags -> 'addr:street', tags -> 'addr:housenumber'),
-                tags -> 'addr:postcode',
-                tags -> 'addr:city'
-            )
-            WHEN country.country_code in ('br') THEN concat_ws(',',
-                concat_ws(' ' , tags -> 'addr:street', tags -> 'addr:housenumber'),
-                tags -> 'addr_district',
-                tags -> 'addr_state',
-                tags -> 'addr_postcode'
-            )
-            WHEN country.country_code in ('ru') THEN concat_ws(',',
-                concat_ws(' ' , tags -> 'addr:street', tags -> 'addr:housenumber'),
-                tags -> 'addr_suburb',
-                tags -> 'addr_district',
-                tags -> 'addr_province',
-                tags -> 'addr_postcode'
-            )
-            ELSE concat_ws(',',
-                concat_ws(' ' , tags -> 'addr:housenumber', tags -> 'addr:street'),
-                tags -> 'addr_city',
-                tags -> 'addr_state',
-                tags -> 'addr_postcode'
-            )
-        END
+    SET country = country.country_code
     FROM public.country_osm_grid as country
-    WHERE ST_Intersects(ST_Transform(points.geometry::geometry, 4326), country.geometry::geometry);
+    WHERE (full_update OR EXISTS(
+        SELECT NULL FROM poi_point.osm_ids WHERE poi_point.osm_ids.osm_id = points.osm_id
+      ))
+      AND country.country_code in ('at', 'de', 'nl', 'no', 'ch', 'pl', 'br', 'ru')
+      AND ST_Transform(points.geometry::geometry, 4326) && country.geometry::geometry;
 
 END;
 $$ LANGUAGE plpgsql;
 
 ALTER TABLE osm_poi_point
-    ADD COLUMN IF NOT EXISTS address text DEFAULT NULL;
+    ADD COLUMN IF NOT EXISTS country text DEFAULT NULL;
 
 CREATE INDEX IF NOT EXISTS osm_poi_point_station_subway_partial_idx ON osm_poi_point (station, subclass)
     WHERE station = 'subway' AND subclass = 'station';
@@ -213,3 +190,38 @@ CREATE CONSTRAINT TRIGGER trigger_refresh
     INITIALLY DEFERRED
     FOR EACH ROW
 EXECUTE PROCEDURE poi_point.refresh();
+
+CREATE OR REPLACE FUNCTION bm_address_osm_poi_point(country_code TEXT, addr_full TEXT, addr_housenumber TEXT, addr_street TEXT, addr_city TEXT, addr_suburb TEXT, addr_district TEXT, addr_province TEXT, addr_state TEXT, addr_postcode TEXT)
+    RETURNS text AS
+$$
+SELECT CASE
+        WHEN addr_full IS NOT NULL THEN addr_full
+        WHEN addr_housenumber IS NULL THEN NULL
+        WHEN country_code in ('at', 'de', 'nl', 'no', 'ch', 'pl') THEN concat_ws(',',
+            concat_ws(' ' , addr_street, addr_housenumber),
+            addr_postcode,
+            addr_city
+        )
+        WHEN country_code in ('br') THEN concat_ws(',',
+            concat_ws(addr_street, addr_housenumber),
+            addr_district,
+            addr_state,
+            addr_postcode
+        )
+        WHEN country_code in ('ru') THEN concat_ws(',',
+            concat_ws(' ' , addr_street, addr_housenumber),
+            addr_suburb,
+            addr_district,
+            addr_province,
+            addr_postcode
+        )
+        WHEN country_code not in ('ru','br','at', 'de', 'nl', 'no', 'ch', 'pl') THEN concat_ws(',',
+            concat_ws(' ' , addr_housenumber, addr_street),
+            addr_city,
+            addr_state,
+            addr_postcode
+        )
+        END;
+
+$$ LANGUAGE SQL IMMUTABLE
+                PARALLEL SAFE;
