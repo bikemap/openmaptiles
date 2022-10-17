7fd5a47d0fdfa00ee95484bbfe80f32ede0c92f9
Benedikt Brandtner <benedikt@bikemap.net>
2022-10-17 03:32:39 +0200

    refactored update_poi_point.sql to partial diff update; added osm_poi_stop_rank_rk_idx on osm_poi_stop_rank to speed up update queries;

--- layers/poi/update_poi_point.sql
+++ layers/poi/update_poi_point.sql
@@ -1,18 +1,32 @@
 DROP TRIGGER IF EXISTS trigger_flag ON osm_poi_point;
 DROP TRIGGER IF EXISTS trigger_refresh ON poi_point.updates;
+DROP TRIGGER IF EXISTS trigger_store ON osm_poi_point;
+
+CREATE SCHEMA IF NOT EXISTS poi_point;
+
+CREATE TABLE IF NOT EXISTS poi_point.osm_ids
+(
+    osm_id bigint PRIMARY KEY
+);
 
 -- etldoc:  osm_poi_point ->  osm_poi_point
-CREATE OR REPLACE FUNCTION update_osm_poi_point() RETURNS void AS
+CREATE OR REPLACE FUNCTION update_osm_poi_point(full_update bool) RETURNS void AS
 $$
 BEGIN
     UPDATE osm_poi_point
     SET subclass = 'subway'
-    WHERE station = 'subway'
+    WHERE (full_update OR EXISTS(
+        SELECT NULL FROM poi_point.osm_ids WHERE poi_point.osm_ids.osm_id = osm_poi_point.osm_id
+      ))
+      AND station = 'subway'
       AND subclass = 'station';
 
     UPDATE osm_poi_point
     SET subclass = 'halt'
-    WHERE funicular = 'yes'
+    WHERE (full_update OR EXISTS(
+        SELECT NULL FROM poi_point.osm_ids WHERE poi_point.osm_ids.osm_id = osm_poi_point.osm_id
+      ))
+      AND funicular = 'yes'
       AND subclass = 'station';
 
     -- ATM without name 
@@ -23,7 +37,10 @@ BEGIN
         COALESCE(tags -> 'operator', tags -> 'network'),
         tags || hstore('name', COALESCE(tags -> 'operator', tags -> 'network'))
     )
-    WHERE subclass = 'atm'
+    WHERE(full_update OR EXISTS(
+        SELECT NULL FROM poi_point.osm_ids WHERE poi_point.osm_ids.osm_id = osm_poi_point.osm_id
+      ))
+      AND subclass = 'atm'
       AND name = ''
       AND COALESCE(tags -> 'operator', tags -> 'network') IS NOT NULL;
 
@@ -41,13 +58,25 @@ BEGIN
 
     UPDATE osm_poi_point
     SET tags = update_tags(tags, geometry)
-    WHERE COALESCE(tags->'name:latin', tags->'name:nonlatin', tags->'name_int') IS NULL
+    WHERE (full_update OR EXISTS(
+        SELECT NULL FROM poi_point.osm_ids WHERE poi_point.osm_ids.osm_id = osm_poi_point.osm_id
+      ))
+      AND COALESCE(tags->'name:latin', tags->'name:nonlatin', tags->'name_int') IS NULL
       AND tags != update_tags(tags, geometry);
 
 END;
 $$ LANGUAGE plpgsql;
 
-SELECT update_osm_poi_point();
+-- Indexes for queries originating from update_osm_poi_point() function
+CREATE INDEX IF NOT EXISTS osm_poi_point_station_subway_partial_idx ON osm_poi_point (station, subclass)
+    WHERE station = 'subway' AND subclass = 'station';
+CREATE INDEX IF NOT EXISTS osm_poi_point_funicular_halt_partial_idx ON osm_poi_point (funicular, subclass)
+    WHERE funicular = 'yes' AND subclass = 'station';
+CREATE INDEX IF NOT EXISTS osm_poi_point_atm_name_partial_idx
+    ON osm_poi_point (subclass, name, COALESCE(tags -> 'operator', tags -> 'network'))
+    WHERE subclass = 'atm' AND name = '' AND COALESCE(tags -> 'operator', tags -> 'network') IS NOT NULL;
+
+SELECT update_osm_poi_point(TRUE);
 
 -- etldoc:  osm_poi_stop_rank ->  osm_poi_point
 CREATE OR REPLACE FUNCTION update_osm_poi_point_agg() RETURNS void AS
@@ -85,13 +114,27 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+-- Indexes for queries originating from update_osm_poi_point_agg function
+CREATE INDEX osm_poi_stop_rank_osm_id_idx ON osm_poi_stop_rank (osm_id);
+CREATE INDEX osm_poi_stop_rank_rk_idx ON osm_poi_stop_rank (rk);
+
 ALTER TABLE osm_poi_point
     ADD COLUMN IF NOT EXISTS agg_stop integer DEFAULT NULL;
 SELECT update_osm_poi_point_agg();
 
 -- Handle updates
 
-CREATE SCHEMA IF NOT EXISTS poi_point;
+CREATE OR REPLACE FUNCTION poi_point.store() RETURNS trigger AS
+$$
+BEGIN
+    IF (tg_op = 'DELETE') THEN
+        INSERT INTO poi_point.osm_ids VALUES (OLD.osm_id) ON CONFLICT (osm_id) DO NOTHING;
+    ELSE
+        INSERT INTO poi_point.osm_ids VALUES (NEW.osm_id) ON CONFLICT (osm_id) DO NOTHING;
+    END IF;
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
 
 CREATE TABLE IF NOT EXISTS poi_point.updates
 (
@@ -113,10 +156,17 @@ DECLARE
     t TIMESTAMP WITH TIME ZONE := clock_timestamp();
 BEGIN
     RAISE LOG 'Refresh poi_point';
-    PERFORM update_osm_poi_point();
+
+    -- Analyze tracking and source tables before performing update
+    ANALYZE poi_point.osm_ids;
+    ANALYZE osm_poi_point;
+
+    PERFORM update_osm_poi_point(FALSE);
     REFRESH MATERIALIZED VIEW osm_poi_stop_centroid;
     REFRESH MATERIALIZED VIEW osm_poi_stop_rank;
     PERFORM update_osm_poi_point_agg();
+    -- noinspection SqlWithoutWhere
+    DELETE FROM poi_point.osm_ids;
     -- noinspection SqlWithoutWhere
     DELETE FROM poi_point.updates;
 
@@ -124,6 +174,12 @@ BEGIN
     RETURN NULL;
 END;
 $$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_store
+    AFTER INSERT OR UPDATE OR DELETE
+    ON osm_poi_point
+    FOR EACH ROW
+EXECUTE PROCEDURE poi_point.store();
 
 CREATE TRIGGER trigger_flag
     AFTER INSERT OR UPDATE OR DELETE
