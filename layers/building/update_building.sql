a2d021408953160464316ff93cfa719c7ebd788e
Benedikt Brandtner <benedikt@bikemap.net>
2025-10-08 16:13:49 +0200

    postgres 17 support

--- layers/building/update_building.sql
+++ layers/building/update_building.sql
@@ -86,7 +86,8 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql STABLE
                     STRICT
-                    PARALLEL SAFE;
+                    PARALLEL SAFE
+                    SET search_path FROM CURRENT;
 
 
 DROP MATERIALIZED VIEW IF EXISTS osm_building_block_gen1_dup CASCADE;
